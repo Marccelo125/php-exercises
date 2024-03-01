ddba2288428cb12a454df0f9773d6d72ff73c3a4
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-02-29 23:46:58 -0300

    deploy js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
+
 async function gerarExercicios() {
-    const response = await fetch ('./json/exercises.json')
-    const listaExercicios= await response.json()
+  try {
+    const response = await axios.get(
+      "./json/exercises.json"
+    );
+    const listaExercicios = response.data;
 
-    const divLista = document.getElementById("div-lista")
+    const divLista = document.getElementById("div-lista");
 
-    listaExercicios.forEach(exercicio => {
-        divLista.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-start text-white" style="border: none; background-color: rgb(22, 22, 22);">  
-            <div class="ms-2 me-auto">
-            <div id="div-exercicio"></div>
-        </li>
-        `
-        for (link of exercicio.links) {
-            const divExercicio = document.getElementById("div-exercicio")
-            divExercicio.innerHTML += `
-                <a href="${link.link}" target="_blank" class="d-flex text-decoration-none flex-column">Exercício n°${exercicio.links.indexOf(link) + 1}</a>
-            `
-        }
+    listaExercicios.forEach((exercicio, index) => {
+      const divExercicio = document.createElement("div");
+      divExercicio.id = `div-exercicio-${index}`;
+
+      const li = document.createElement("li");
+      li.className =
+        "list-group-item d-flex justify-content-between align-items-start text-white";
+      li.style.border = "none";
+      li.style.backgroundColor = "rgb(22, 22, 22)";
+      li.innerHTML = `
+                <div class="ms-2 me-auto">
+                    <div id="${divExercicio.id}"></div>
+                </div>
+            `;
+
+      divLista.appendChild(li);
+
+      exercicio.links.forEach((link, linkIndex) => {
+        const divExercicioItem = document.getElementById(`${divExercicio.id}`);
+        const anchor = document.createElement("a");
+        anchor.href = link.link;
+        anchor.target = "_blank";
+        anchor.className = "d-flex text-decoration-none flex-column";
+        anchor.innerText = `Exercício n°${linkIndex + 1}`;
+
+        divExercicioItem.appendChild(anchor);
+      });
     });
-    gerarExercicios()
+  } catch (error) {
+    console.error("Erro ao obter os exercícios:", error);
+  }
 }
+
+gerarExercicios();
