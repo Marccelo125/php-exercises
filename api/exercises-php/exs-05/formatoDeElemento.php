62e42d9931aa12bde6b7e4480f2345aabe808ef2
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-03-12 02:09:05 -0300

    📦docs: exs 5 - 02

---
+++ api/exercises-php/exs-05/formatoDeElemento.php
@@ -0,0 +1,66 @@
+<?php
+
+abstract class formatoDoElemento {
+    abstract public function desenhar();
+    abstract public function redimensionar($raio, $comprimento, $altura);
+}
+
+class Retangulo extends formatoDoElemento {
+    public $comprimento;
+    public $altura;
+
+    public function __construct($comprimento, $altura) {
+        $this->comprimento = $comprimento;
+        $this->altura = $altura;
+    }
+
+    public function desenhar() {
+        $areaTotal = $this->comprimento * $this->altura;
+        $printComprimento = str_repeat("🟥", $this->comprimento);
+    
+        for ($i = 0; $i < $this->altura; $i++) {
+            echo $printComprimento . "<br />";
+        }
+
+        echo "<br /> Comprimento: {$this->comprimento} <br />";
+        echo "Altura: {$this->altura} <br />";
+        echo "Área: $areaTotal <br />";
+    }
+
+    public function redimensionar($comprimento, $altura, $raio = 0) {
+        $this->comprimento = $comprimento;
+        $this->altura = $altura;
+    }
+}
+
+class Circulo extends formatoDoElemento {
+    public $raio;
+
+    public function __construct($raio) {
+        $this->raio = $raio;
+    }
+
+    public function desenhar() {
+        $areaTotal = pi() * pow($this->raio, 2);
+        $raioPow = pow($this->raio, 2);
+        
+        echo "<div style='width:{$raioPow}; height:{$raioPow}; border-radius:50%; background-color:red'></div> <br />";
+
+        echo "<br /> Raio: {$this->raio} <br />";
+        echo "Área: $areaTotal <br />";
+    }
+
+    public function redimensionar($raio, $comprimento = 0, $altura = 0) {
+        $this->raio = $raio;
+    }
+}
+
+$retangulo = new Retangulo(5, 10);
+$retangulo->desenhar();
+$retangulo->redimensionar(10, 10);
+$retangulo->desenhar();
+
+$circulo = new Circulo(5);
+$circulo->desenhar();
+$circulo->redimensionar(8);
+$circulo->desenhar();
