713df8e17473a4536ac97e8e78a0bd3c466e2d77
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-03-13 20:43:38 -0300

    ♻️refactor: index structure ex-03

--- api/index-exs-03.php
+++ api/index-exs-03.php
@@ -46,6 +46,31 @@ require_once('exercises-php/exs-03/carro.php');
                 </div>
                 <div class="col-12 col-md-6 col-lg-6 my-2 overflow-auto shadow p-4" style="max-height: 75vh; border: 2px solid #1c1c1c">
                     <?php
+                    class contador
+                    {
+                        public $contador;
+
+                        public function zerar()
+                        {
+                            $this->contador = 0;
+                            echo "<br /> Valor do contador agora é: $this->contador";
+                        }
+
+                        public function incrementar()
+                        {
+                            $this->contador += 1;
+
+                            echo "<br />";
+                            echo "Valor do contador agora é: $this->contador";
+                            echo "<br />";
+                        }
+
+                        public function valorContador()
+                        {
+                            echo "Valor do contador é: $this->contador";
+                        }
+                    }
+
                     $meuContador = new contador();
 
                     $meuContador->incrementar();
@@ -77,6 +102,31 @@ require_once('exercises-php/exs-03/carro.php');
                 </div>
                 <div class="col-12 col-md-6 col-lg-6 my-2 overflow-auto shadow p-4" style="max-height: 75vh; border: 2px solid #1c1c1c">
                     <?php
+                    class moldarBola
+                    {
+                        public $cor;
+                        public $circunferencia;
+                        public $material;
+
+                        public function __construct($cor, $circunferencia, $material)
+                        {
+                            $this->cor = $cor;
+                            $this->circunferencia = $circunferencia;
+                            $this->material = $material;
+                        }
+
+                        public function trocarCor($novaCor)
+                        {
+                            $this->cor = $novaCor;
+                            echo "<br /> Troquei a cor da bola para: $this->cor <br />";
+                        }
+
+                        public function mostrarCor()
+                        {
+                            echo "<br /> A cor da bola é: $this->cor <br />";
+                        }
+                    }
+
                     $novaBola = new moldarBola("Vermelho", "25", "Metal");
 
                     $novaBola->mostrarCor();
@@ -106,6 +156,64 @@ require_once('exercises-php/exs-03/carro.php');
                 </div>
                 <div class="col-12 col-md-6 col-lg-6 my-2 overflow-auto shadow p-4" style="max-height: 75vh; border: 2px solid #1c1c1c">
                     <?php
+                    class contaCorrente
+                    {
+                        public $numConta;
+                        public $nomeCorrentista;
+                        public $saldo;
+
+                        public function __construct($numConta, $nomeCorrentista, $saldo = 0)
+                        {
+                            if ($saldo < 0) {
+                                echo "Você não pode criar uma conta com saldo negativo";
+                                return;
+                            }
+                            $this->numConta = $numConta;
+                            $this->nomeCorrentista = $nomeCorrentista;
+                            $this->saldo = $saldo;
+                        }
+
+                        public function mostrarConta()
+                        {
+                            echo "<br /> N° da Conta: $this->numConta";
+                            echo "<br /> Nome Corretista: $this->nomeCorrentista";
+                            echo "<br /> Saldo: $this->saldo";
+                        }
+
+                        public function novoNome($nome)
+                        {
+                            if (strlen($nome) >= 3) {
+                                $this->nomeCorrentista = $nome;
+                                echo "Seu nome agora é: $this->nomeCorrentista";
+                            } else {
+                                echo "O novo nome deve conter no minímo 3 caracteres";
+                                return;
+                            }
+                        }
+
+                        public function sacar($valor)
+                        {
+                            if ($valor > $this->saldo) {
+                                echo "<br />Saldo insuficiente para saque";
+                                return;
+                            }
+                            $this->saldo -= $valor;
+                            echo "<br />Sacou: $valor";
+                            echo "<br />Saldo atual: $this->saldo";
+                        }
+
+                        public function deposito($valor)
+                        {
+                            if ($valor > 0) {
+                                $this->saldo += $valor;
+                                echo "<br />Deposito: $valor";
+                                echo "<br />Saldo atual: $this->saldo";
+                            } else {
+                                echo "O valor inserido é INVÁLIDO";
+                            }
+                        }
+                    }
+
                     $novaConta = new contaCorrente(10, "Marcelo", 100);
                     $novaConta->novoNome("João");
                     $novaConta->mostrarConta();
@@ -131,6 +239,55 @@ require_once('exercises-php/exs-03/carro.php');
                 </div>
                 <div class="col-12 col-md-6 col-lg-6 my-2 overflow-auto shadow p-4" style="max-height: 75vh; border: 2px solid #1c1c1c">
                     <?php
+                    class calculadora
+                    {
+                        public $historico = [];
+
+                        public function somar($valor1, $valor2)
+                        {
+                            $resultado = $valor1 + $valor2;
+
+                            echo "<br /> Somando os valores: $valor1 e $valor2 o resultado é $resultado <br />";
+                            $this->addHistorico("Operação: $valor1 + $valor2 = $resultado <br />");
+                        }
+
+                        public function diminuir($valor1, $valor2)
+                        {
+                            $resultado = $valor1 - $valor2;
+
+                            echo "<br /> Subtraindo os valores: $valor1 e $valor2 o resultado é $resultado <br />";
+                            $this->addHistorico("Operação: $valor1 - $valor2 = $resultado <br />");
+                        }
+
+                        public function multiplicar($valor1, $valor2)
+                        {
+                            $resultado = $valor1 * $valor2;
+
+                            echo "<br /> Multiplicando os valores: $valor1 por $valor2 o resultado é $resultado <br />";
+                            $this->addHistorico("Operação: $valor1 x $valor2 = $resultado <br />");
+                        }
+
+                        public function dividir($valor1, $valor2)
+                        {
+                            $resultado = $valor1 / $valor2;
+
+                            echo "<br /> Dividindo os valores: $valor1 por $valor2 o resultado é $resultado <br />";
+                            $this->addHistorico("Operação: $valor1 / $valor2 = $resultado <br />");
+                        }
+
+                        public function addHistorico($dados)
+                        {
+                            $this->historico[] = $dados;
+                        }
+
+                        public function mostrarHistorico()
+                        {
+                            foreach ($this->historico as $key => $value) {
+                                echo "$value";
+                            }
+                        }
+                    }
+
                     $novoCalculo = new  calculadora();
                     $novoCalculo->somar(1, 2);
                     $novoCalculo->diminuir(2, 1);
@@ -171,6 +328,74 @@ require_once('exercises-php/exs-03/carro.php');
                 </div>
                 <div class="col-12 col-md-6 col-lg-6 my-2 overflow-auto shadow p-4" style="max-height: 75vh; border: 2px solid #1c1c1c">
                     <?php
+                    class bombaCombustivel
+                    {
+
+                        public $tipoCombustivel;
+                        public $valorLitro;
+                        public $quantidadeCombustivel;
+
+                        public function __construct($tipoCombustivel, $quantidadeCombustivel)
+                        {
+
+                            $this->tipoCombustivel = $tipoCombustivel;
+                            $this->quantidadeCombustivel = $quantidadeCombustivel;
+
+                            if ($this->tipoCombustivel == 'GASOLINA') {
+                                $this->alterarValor(5);
+                            }
+                            if ($this->tipoCombustivel == 'ALCOOL') {
+                                $this->alterarValor(3);
+                            }
+                            if ($this->tipoCombustivel == 'DIESEL') {
+                                $this->alterarValor(4);
+                            } else return;
+                        }
+
+                        public function abastecerPorValor($valor)
+                        {
+                            if ($valor > 0) {
+                                $total = round($valor / $this->valorLitro, 2);
+                                echo "<br /> Você deseja abastecer R$ $valor";
+
+                                if ($this->quantidadeCombustivel >= $total) {
+                                    echo "<br /> Você abasteceu $total Litros";
+                                    $this->quantidadeCombustivel -= $total;
+                                } else {
+                                    echo "Quantidade indisponível <br />";
+                                }
+                            }
+                        }
+
+                        public function abastecerPorLitro($valor)
+                        {
+                            $total =  round($this->valorLitro * $valor, 2);
+                            echo "<br /> Você deseja abastecer R$ $valor";
+                            if ($this->quantidadeCombustivel >= $valor) {
+                                echo "<br /> Você abasteceu $total Litros";
+                                $this->quantidadeCombustivel -= $valor;
+                            } else {
+                                echo ". Quantidade indisponível<br>";
+                            }
+                        }
+
+                        public function alterarValor($novoValor)
+                        {
+                            $this->valorLitro = round($novoValor, 2);
+                            echo "<br /> Valor de combustível alterado para R$ $novoValor";
+                        }
+                        public function alterarTipoCombustivel($novoCombustivel)
+                        {
+                            $this->tipoCombustivel = $novoCombustivel;
+                            echo "<br /> Tipo de combustível alterado para $novoCombustivel";
+                        }
+                        public function alterarQntdCombustivel($novaQntde)
+                        {
+                            $this->quantidadeCombustivel = round($novaQntde, 2);
+                            echo "<br /> Quantidade de combustível total alterado para $novaQntde Litros";
+                        }
+                    }
+
                     $novaBomba = new bombaCombustivel("GASOLINA", 10000);
                     $novaBomba->abastecerPorValor(50);
                     $novaBomba->abastecerPorLitro(50);
@@ -204,10 +429,47 @@ require_once('exercises-php/exs-03/carro.php');
                 </div>
                 <div class="col-12 col-md-6 col-lg-6 my-2 overflow-auto shadow p-4" style="max-height: 75vh; border: 2px solid #1c1c1c">
                     <?php
-                        $novoCarro = new carro("Fusca", 10, 100);
-                        $novoCarro -> andar(10);
-                        $novoCarro -> obterGasolina();
-                        $novoCarro -> adicionarGasolina(50);
+                    class carro
+                    {
+                        public function __construct(
+                            public $nomeCarro,
+                            public $consumo,
+                            public $combustivel = 0,
+                        ) {
+                            $this->nomeCarro = $nomeCarro;
+                            $this->consumo = $consumo;
+                            $this->combustivel = $combustivel;
+                        }
+
+                        public function andar($distancia)
+                        {
+                            $mediaConsumo = $this->combustivel * $this->consumo;
+                            if ($mediaConsumo >= $distancia) {
+                                $this->combustivel -= round($this->consumo / $distancia, 2);
+                                echo "<br /> Seu carro $this->nomeCarro percorreu uma distância de: $distancia km. Seu tanque possuí $this->combustivel Litros";
+                            }
+                        }
+
+                        public function obterGasolina()
+                        {
+                            echo "<br /> Seu carro $this->nomeCarro possuí $this->combustivel Litros diponíveis no tanque.";
+                        }
+
+                        public function adicionarGasolina($qntdDeLitros)
+                        {
+                            if ($qntdDeLitros > 0) {
+                                $this->combustivel += $qntdDeLitros;
+                                echo "<br /> Você abasteceu $qntdDeLitros Litros! Agora seu carro tem $this->combustivel litros.";
+                            } else {
+                                echo "<br /> Quantidade inválida";
+                            }
+                        }
+                    }
+                    
+                    $novoCarro = new carro("Fusca", 10, 100);
+                    $novoCarro->andar(10);
+                    $novoCarro->obterGasolina();
+                    $novoCarro->adicionarGasolina(50);
                     ?>
                 </div>
             </div>
